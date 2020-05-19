64ca3a39512bfe9741a5fd0b97734f0e2ae71642
Louis Pham <louispham23@gmail.com>
2020-05-19 11:14:33 -0700

    added section and a scroll view

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -23,6 +23,36 @@ class _HomeState extends State<Home> {
     ),
   ];
 
+  List<Event> eventsThisWeek = [
+    Event(
+      title: "Bastketball",
+      location: "CSUS main gym",
+      dateTime: DateTime.now(),
+      img: 'assets/images/basket.jpeg',
+    ),
+    Event(
+      title: "Bastketball",
+      location: "CSUS main gym",
+      dateTime: DateTime.now(),
+      img: 'assets/images/basket.jpeg',
+    ),
+  ];
+
+  List<Event> eventsThisMonth = [
+    Event(
+      title: "Food Pantry",
+      location: "Library Quads",
+      dateTime: DateTime.now(),
+      img: 'assets/images/food.jpg',
+    ),
+    Event(
+      title: "Food Pantry",
+      location: "Library Quads",
+      dateTime: DateTime.now(),
+      img: 'assets/images/food.jpg',
+    ),
+  ];
+
   Widget _buildToday() {
     return Container(
       width: double.infinity,
@@ -34,16 +64,54 @@ class _HomeState extends State<Home> {
           Text('Today', style: kRegTitle),
           Text('Features', style: kBoldTitle),
           Container(
-            // child: ListView(
-            //   scrollDirection: Axis.horizontal,
-            //   children: events.toList(),
-
+            // child: Event(
+            //   dateTime: DateTime.now(),
+            //   title: "Testing Card",
+            //   img: 'assets/images/music.jpeg',
+            //   location: '123 schill street',
             // ),
-            child: Event(
-              dateTime: DateTime.now(),
-              title: "Testing Card",
-              img: 'assets/images/music.jpeg',
-              location: '123 schill street',
+            child: Row(
+              children: events.toList(),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildThisWeek() {
+    return Container(
+      width: double.infinity,
+      color: Colors.white,
+      padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text('This Week', style: kRegTitle),
+          Text('Events', style: kBoldTitle),
+          Container(
+            child: Row(
+              children: eventsThisWeek.toList(),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildThisMonth() {
+    return Container(
+      width: double.infinity,
+      color: Colors.white,
+      padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text('This Month', style: kRegTitle),
+          Text('Events', style: kBoldTitle),
+          Container(
+            child: Row(
+              children: eventsThisMonth.toList(),
             ),
           ),
         ],
@@ -53,10 +121,14 @@ class _HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: <Widget>[
-        _buildToday(),
-      ],
+    return SingleChildScrollView(
+      child: Column(
+        children: <Widget>[
+          _buildToday(),
+          _buildThisWeek(),
+          _buildThisMonth(),
+        ],
+      ),
     );
   }
 }
--- lib/services/event.dart
+++ lib/services/event.dart
@@ -13,49 +13,51 @@ class Event extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       width: 149,
-      margin: EdgeInsets.symmetric(
-        vertical: 20,
-      ),
+      margin: EdgeInsets.fromLTRB(0, 20, 20, 0),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
-          ClipRRect(
-            borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(10),
-              topRight: Radius.circular(10),
-            ),
-            child: Image.asset(
-              img,
-              height: 144,
-              fit: BoxFit.fill,
-            ),
-          ),
-          Container(
-            padding: EdgeInsets.symmetric(vertical: 10),
-            width: double.infinity,
-            decoration: BoxDecoration(
-              color: gray300,
-              borderRadius: BorderRadius.only(
-                bottomLeft: Radius.circular(10),
-                bottomRight: Radius.circular(10),
+          Stack(
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.only(top: 20, bottom: 10),
+                margin: EdgeInsets.only(top: 130),
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: gray300,
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(10),
+                    bottomRight: Radius.circular(10),
+                  ),
+                ),
+                child: Column(
+                  children: <Widget>[
+                    Text(
+                      title,
+                      style: kCardTitle,
+                    ),
+                    Text(
+                      location,
+                      textAlign: TextAlign.center,
+                      style: kCardLocation,
+                    ),
+                    Text(
+                      dateTime.hour.toString(),
+                      style: kCardTime,
+                    ),
+                  ],
+                ),
               ),
-            ),
-            child: Column(
-              children: <Widget>[
-                Text(
-                  title,
-                  style: kCardTitle,
+              ClipRRect(
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+                child: Image.asset(
+                  img,
+                  height: 140,
+                  width: double.infinity,
+                  fit: BoxFit.fill,
                 ),
-                Text(
-                  location,
-                  style: kCardLocation,
-                ),
-                Text(
-                  dateTime.hour.toString(),
-                  style: kCardTime,
-                ),
-              ],
-            ),
+              ),
+            ],
           ),
         ],
       ),
